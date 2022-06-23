40299b232ef217b696397416f575cfd657cda5d0
AlexKrivets <krivets2005@gmail.com>
2022-06-23 15:39:55 +0300

    feat: add popupt

--- index.js
+++ index.js
@@ -57,14 +57,75 @@ function popupClose(popupActive, doUnlock = true) {
 function bodyLock() {
     const lockPaddingValue =
         window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-    for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = lockPaddingValue;
+
+    if (lockPadding.length > 0) {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+        }
     }
     body.style.paddingRight = lockPaddingValue;
     body.classList.add('lock');
+
     unlock = false;
     setTimeout(function() {
         unlock = true;
     }, timeout);
 }
+
+function bodyUnLock() {
+    setTimeout(function() {
+        if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = '0px';
+            }
+        }
+        body.style.paddingRight = '0px';
+        body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function() {
+        unlock = true;
+    }, timeout);
+}
+// document.addEventListener('keydown', function(e) {
+//     if (e.which === 27) {
+//         const popupActive = document.querySelector('.popup.open');
+//         popupClose(popupActive);
+//     }
+// });
+
+// ____________
+// tel
+var phoneInput = document.querySelector('.phone');
+phoneInput.addEventListener('keydown', function(event) {
+    if (!(
+            event.key == 'ArrowLeft' ||
+            event.key == 'ArrowRight' ||
+            event.key == 'Backspace' ||
+            event.key == 'Tab'
+        )) {
+        event.preventDefault();
+    }
+    var mask = '+7 (111) 111-11-11';
+
+    if (/[0-9\+\ \-\(\)]/.test(event.key)) {
+        var currentString = this.value;
+        var currentLength = currentString.length;
+        if (/[0-9]/.test(event.key)) {
+            if (mask[currentLength] == '1') {
+                this.value = currentString + event.key;
+            } else {
+                for (var i = currentLength; i < mask.length; i++) {
+                    if (mask[i] == '1') {
+                        this.value = currentString + event.key;
+                        break;
+                    }
+                    currentString += mask[i];
+                }
+            }
+        }
+    }
+});
